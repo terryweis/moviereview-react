cecd772ce278e5f3af015a793b72fcb8907e55a6
terryweis <weist2088@gmail.com>
2024-06-28 15:08:37 -0500

    Trailer and Review added, but not working

--- src/App.js
+++ src/App.js
@@ -1,47 +1,74 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import axios from 'axios'
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
-  
-  const[movies, setMovies] = useState();
 
-  const getMovies = async () => {
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
+
+  const getMovies = async () =>{
     
     try
     {
-      const response = await axios.get("http://localhost:8080/api/v1/movies");
 
-      console.log(response.data);
+      const response = await api.get("http://localhost:8080/api/v1/movies");
 
-
-    setMovies(response.data);
+      setMovies(response.data);
 
     } 
-    catch(err){
+    catch(err)
+    {
       console.log(err);
     }
-    
+  }
+
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`http://localhost:8080/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
 
   }
 
   useEffect(() => {
     getMovies();
   },[])
-  
+
   return (
     <div className="App">
-     
-<Routes>
-  <Route path="/" element={<Layout/>}>
-      <Route path="/" element={<Home movies={movies} />} ></Route>
+    <Header/>
+    <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies} />} ></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+          <Route path="*" element = {<NotFound/>}></Route>
+        </Route>
+    </Routes>
 
-  </Route>
-</Routes>
-    </div>
+  </div>
   );
 }
 
